60d4302da24fd06cae292cb28153c565a275fdd6
Martín <magadiflo@gmail.com>
2022-05-27 12:35:23 -0500

    Inicio del tutorial: ' Spring Boot and Spring Security with JWT including Access and Refresh Tokens'

---
+++ README.md
@@ -0,0 +1,2 @@
+## Información
+Tomado de: https://www.youtube.com/watch?v=VVn9OG9nfH0&t=1069s
---
+++ src/main/java/com/magadiflo/app/SpringBootJwtAmigosCodeApplication.java
@@ -0,0 +1,13 @@
+package com.magadiflo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtAmigosCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtAmigosCodeApplication.class, args);
+	}
+
+}
